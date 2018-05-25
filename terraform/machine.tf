ed8ed86bba6cf68470ff4568785cb69ef364f073
Michael Schubert <michael@kinvolk.io>
2018-05-25 11:50:13 +0200

    terraform: add cloud-init script

--- terraform/machine.tf
+++ terraform/machine.tf
@@ -8,4 +8,5 @@ resource "hcloud_server" "bpf-playground" {
   server_type = "cx11"
   ssh_keys = "${var.hcloud_ssh_keys_names}"
   datacenter = "fsn1-dc8"
+  user_data = "${file("user-data.bash")}"
 }
---
+++ terraform/user-data.bash
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -euo pipefail
+
+adduser --disabled-password ubuntu
+usermod -a -G sudo ubuntu
+echo "%sudo ALL=(ALL) NOPASSWD:ALL" >>/etc/sudoers.d/10-sudo-group-nopasswd
+
+apt-get update
+apt-get upgrade -y
+
+apt-get install -y \
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  git \
+  make \
+  python3-venv \
+  screen \
+  software-properties-common \
+  tmux
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
+add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) \
+  edge"
+
+apt-get update
+
+apt-get install -y docker-ce
+
+usermod -a -G docker ubuntu
